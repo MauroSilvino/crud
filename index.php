2130d81bbaaf537fe61849af944c72b843b0626c
Mauro Silvino <67653716+MauroSilvino@users.noreply.github.com>
2020-07-18 16:04:00 -0300

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,7 @@ include_once 'includes/message.php';
 
             <tbody>
                 <?php
+                //Coleta de dados no banco.
                 $sql = "SELECT * FROM clientes";
                 $resultado = mysqli_query($connect, $sql);
 
@@ -33,19 +34,17 @@ include_once 'includes/message.php';
                     <td><a href="editar.php?id=<?php echo $dados['id']; ?>" class="btn-floating orange"><i class="material-icons">edit</i></a></td>
                     <td><a href="#modal<?php echo $dados['id']; ?>" class="btn-floating red modal-trigger"><i class="material-icons">delete</i></a></td>
                     <div id="modal<?php echo $dados['id']; ?>" class="modal">
-            <div class="modal-content">
-             <h4>Opa!</h4>
-             <p>Tem certeza que deseja excluir esse cliente?</p>
-         </div>
-            
-            <div class="modal-footer">
-            <form action="php_action/delete.php" method="POST">
-            <input type="hidden" name="id" value="<?php echo $dados['id']; ?>">
-            <button type="submit" name="btn-deletar" class="btn red"> Sim, quero deletar.</button>
-            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancelar</a>
-            </form>
-            </div>
-                
+                        <div class="modal-content">
+                             <h4>Opa!</h4>
+                            <p>Tem certeza que deseja excluir esse cliente?</p>
+                        </div>
+                        <div class="modal-footer">
+                            <form action="php_action/delete.php" method="POST">
+                                <input type="hidden" name="id" value="<?php echo $dados['id']; ?>">
+                                <button type="submit" name="btn-deletar" class="btn red"> Sim, quero deletar.</button>
+                                <a href="#!" class="modal-close waves-effect waves-green btn-flat">Cancelar</a>
+                            </form>
+                        </div>
                 </tr>
                 <?php 
                 endwhile;
@@ -55,14 +54,8 @@ include_once 'includes/message.php';
         </table>
         <br>
         <a href="adicionar.php" class="btn">Adicionar Cliente</a>
-        
      </div>
 </div>
-
-
-
-
-
 
 <?php
 include_once 'includes/footer.php';
